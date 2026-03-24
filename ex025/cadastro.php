10e746919532f34ae3bc499aa55c07f9a00ace9c
Cecilia Silagi <cecilia@silagi-web.net>
2026-03-24 13:08:31 +0100

    Update

--- ex025/cadastro.php
+++ ex025/cadastro.php
@@ -1,9 +1,9 @@
 Os dados foram enviados!
 Aprenda PHP para saber o que fazer com eles.
 <?php 
-    $nome = $_GET["inome"];
-    $sobrenome = $_GET["isobrenome"];
-    $iusu = $_GET["iusu"];
+    $nome = $_GET["nome"];
+    $sobrenome = $_GET["sobrenome"];
+    $iusu = $_GET["usu"];
     echo ".Olá $nome $sobrenome $iusu, seja bem-vindo(a)!";
 
 ?>
