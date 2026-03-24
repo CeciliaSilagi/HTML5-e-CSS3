91aa663105fdf46dc9570e1015b7603858aa9e88
Cecilia Silagi <cecilia@silagi-web.net>
2026-03-24 13:42:24 +0100

    Update

--- ex025/cadastro.php
+++ ex025/cadastro.php
@@ -5,6 +5,7 @@ Aprenda PHP para saber o que fazer com eles.
     $nome = $_GET["nome"];
     $sobrenome = $_GET["sobrenome"];
     $usu = $_GET["usu"];
+    $senha = $_GET["senha"];
     $media = $_GET["media"];
     echo ".Olá $nome $sobrenome $usu, seja bem-vindo(a)!";
 
